02d59f57f42e93a3d71c25f97172cab9979c738d
Unknown <federico.alberti.gurnerio@gmail.com>
2019-06-19 20:21:37 -0300

    Models and admin

--- SistDistCore/admin.py
+++ SistDistCore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from SistDistCore.models import Voter, Vote, Circuit, VoteCenter, ElectoralCort, Candidate, PoliticalParty
 
 # Register your models here.
+
+admin.site.register(Voter)
+admin.site.register(Vote)
+admin.site.register(Circuit)
+admin.site.register(VoteCenter)
+admin.site.register(ElectoralCort)
+admin.site.register(Candidate)
+admin.site.register(PoliticalParty)
